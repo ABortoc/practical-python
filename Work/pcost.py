d71ca1f8cb03fe4670f3acc440d8ac1f2126f549
abortoc <abortoc@gmail.com>
2021-05-18 17:57:13 -0400

    with csv library

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,20 +1,30 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
 
 def portfolio_cost(filename):
     sum = 0
     
+    # with open(filename, 'rt') as file:
+    #     for line in file:
+    #         row = line.split(',')
+    #         if row[1].isalpha() is False and row[2][:-1].isalpha() is False:
+    #             try:
+    #                 sum += int(row[1]) * float(row[2][:-1])
+    #             except ValueError:
+    #                 print('Invalid input')
+    
     with open(filename, 'rt') as file:
-        for line in file:
-            row = line.split(',')
-            if row[1].isalpha() is False and row[2][:-1].isalpha() is False:
-                try:
-                    sum += int(row[1]) * float(row[2][:-1])
-                except ValueError:
-                    print('Invalid input')
+        rows = csv.reader(file)
+        headers = next(rows)
+        for row in rows:
+            try:
+                sum += int(row[1]) * float(row[2])
+            except ValueError:
+                print('Invalid value')
     
     return sum
 
-cost = portfolio_cost('Data/portfolio.csv')
+cost = portfolio_cost('Work/Data/portfolio.csv')
 print('Total cost:', cost)
