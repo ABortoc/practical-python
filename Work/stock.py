c5d3e1d5d1d4616fb713915a8db3473c052791fc
abortoc <abortoc@gmail.com>
2021-06-22 17:15:49 -0400

    minor code tweaks to Stock class

--- Work/stock.py
+++ Work/stock.py
@@ -1,6 +1,8 @@
 # stock.py
 
 class Stock:
+    __slots__ = ('name','_shares','price')
+    
     def __init__(self, name, shares, price) -> None:
         self.name = name
         self.shares = shares
@@ -8,7 +10,18 @@ class Stock:
     
     def __repr__(self) -> str:
         return f'Stock({self.name}, {self.shares}, {self.price})'
-        
+    
+    @property
+    def shares(self):
+        return self._shares
+    
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, int):
+            raise TypeError('int value is expected')
+        self._shares = value
+    
+    @property    
     def cost(self) -> float:
         return self.shares * self.price
     
